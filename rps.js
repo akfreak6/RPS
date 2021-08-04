bed3808ea4b70adb836e561bd08071edefbe1369
akfreak6 <akfreak6@gmail.com>
2021-08-04 10:12:38 +0530

    w/o final game count

--- rps.js
+++ rps.js
@@ -1,40 +1,56 @@
 var items = ['rock', 'paper', 'scissor'];                           //init things to play with
+var p = 0, c = 0, i = 0, t = 0;  
 
 function computerPlay(items){                                       //computer side function
     return items[Math.floor(Math.random()*items.length)];
 }
 
-const playerSelection = prompt("Input Your Selection!").toLowerCase();
-//console.log(playerSelection);
-const computerSelection = computerPlay(items); 
-//console.log(computerSelection)
- 
-for (i=5; i>=p; i++){
-function playRound(playerSelection, computerSelection) 
+
+    function playRound() 
 {
-    if  ((playerSelection == 'rock' && computerSelection == 'paper')    ||
-        (playerSelection== "rock" && computerSelection == "scissor")    ||
-        (playerSelection == "scissor" && computerSelection == "paper")
-        ){
-        console.log("You Win! "+ playerSelection + " beats " + computerSelection +"!")
-        p+1;
+    let playerSelection = prompt("Input Your Selection!").toLowerCase();
+    let computerSelection = computerPlay(items); 
+    if  (   (playerSelection == 'paper' && computerSelection == 'rock')    ||
+            (playerSelection== "rock" && computerSelection == "scissor")    ||
+            (playerSelection == "scissor" && computerSelection == "paper")
+        )
+    {
+        console.log("You Win! "+ playerSelection + " beats " + computerSelection +"!");
+        p++;
+        i++;
+        game()
     } 
-    else if ( playerSelection === computerSelection){
+    else if ( playerSelection === computerSelection)
+    {
         console.log("Tie, no points awarded.")
+        i++;
+        game()
     }
-    else{
+    else 
+    {
         console.log("You Lose!"+ computerSelection +" beats "+playerSelection+"!")
-        c+1;
+        c++;
+        i++;
+        game()
     }
-    // else if(playerSelection== "rock" && computerSelection == "scissor"){
-    //     console.log("You win! Paper beats Rock!")
-    // }
-    // else if(playerSelection == "scissor" && computerSelection == "rock"){
-    //     console.log("You Lose! Paper beats Rock!")
-    // }
-    // else if(playerSelection == "scissor" && computerSelection == "paper"){
-    //     console.log("You win! Paper beats Rock!")
-    // }
 }
-playRound(playerSelection, computerSelection)
+function game()
+{
+    while( i <= 4 ){
+        console.log(i);
+        playRound()
+    }
+    if((i === 4) && (p>c)){
+        console.log("You win bish!!")
+    }
+    else if((i === 4) && (c>p)){
+        console.log("Computer Wins beyotch!")
+    }
 }
+game()
+// if((i === 4) && (p>c)){
+//     console.log("You win bish!!")
+// }
+// else if((i === 4) && (c>p)){
+//     console.log("Computer Wins beyotch!")
+// }
